5ca938441f3aea42fd0dcd734f0161b4c5fa54e5
katsew <y.katsew@gmail.com>
2016-12-26 12:39:17 +0900

    move to bin

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-#!/usr/bin/env node
-
 'use strict';
 
-const program = require('commander');
-const path = require('path');
 const vfs = require('vinyl-fs');
 const xlsx2csv = require('./lib/xlsx2csv.js');
 
-program
-  .version('0.0.1')
-  .option('-s, --source [src]', 'add source')
-  .option('-o, --output [dest]', 'output destination')
-  .parse(process.argv);
-
-if (program.source && program.output) {
-
-  var src = path.resolve(program.source);
-  var dest = path.resolve(program.output);
+module.exports = function (src, dest) {
   try {
     vfs.src(src)
       .pipe(xlsx2csv({
@@ -27,4 +14,4 @@ if (program.source && program.output) {
   } catch (e) {
     console.log(e);
   }
-}
+};
